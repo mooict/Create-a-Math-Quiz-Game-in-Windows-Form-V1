c949141a16018e490b90db46d2b4d453536449d1
MOOICT <MOOICT@DESKTOP-OQHBRE0>
2022-03-04 11:11:08 +0000

    Add project files.

---
+++ Maths Quiz 01 MOOICT/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Maths_Quiz_01_MOOICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblNumA = new System.Windows.Forms.Label();
+            this.lblSymbol = new System.Windows.Forms.Label();
+            this.lblNumB = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.ForeColor = System.Drawing.Color.Maroon;
+            this.lblScore.Location = new System.Drawing.Point(13, 13);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(47, 13);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblNumA
+            // 
+            this.lblNumA.AutoSize = true;
+            this.lblNumA.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumA.Location = new System.Drawing.Point(13, 41);
+            this.lblNumA.Name = "lblNumA";
+            this.lblNumA.Size = new System.Drawing.Size(49, 33);
+            this.lblNumA.TabIndex = 1;
+            this.lblNumA.Text = "00";
+            // 
+            // lblSymbol
+            // 
+            this.lblSymbol.AutoSize = true;
+            this.lblSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSymbol.Location = new System.Drawing.Point(87, 41);
+            this.lblSymbol.Name = "lblSymbol";
+            this.lblSymbol.Size = new System.Drawing.Size(33, 33);
+            this.lblSymbol.TabIndex = 2;
+            this.lblSymbol.Text = "+";
+            // 
+            // lblNumB
+            // 
+            this.lblNumB.AutoSize = true;
+            this.lblNumB.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumB.Location = new System.Drawing.Point(146, 41);
+            this.lblNumB.Name = "lblNumB";
+            this.lblNumB.Size = new System.Drawing.Size(49, 33);
+            this.lblNumB.TabIndex = 3;
+            this.lblNumB.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(216, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "=";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.Color.Teal;
+            this.lblAnswer.Location = new System.Drawing.Point(15, 92);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(58, 20);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "correct";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.Location = new System.Drawing.Point(255, 41);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(71, 40);
+            this.txtAnswer.TabIndex = 6;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.CheckAnswer);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(345, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CheckButtonClickEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 132);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNumB);
+            this.Controls.Add(this.lblSymbol);
+            this.Controls.Add(this.lblNumA);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Maths Quiz Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblNumA;
+        private System.Windows.Forms.Label lblSymbol;
+        private System.Windows.Forms.Label lblNumB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
